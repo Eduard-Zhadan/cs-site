e4fbba280237829009675ba0c909cc33228085b4
Eduard Zhadan <edik4jadan@gmail.com>
2025-05-04 22:20:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// function toggleWeaponFilter() {
+//   let filterList = document.getElementById("weapon-type");
+//   let toggleBtn = document.getElementById("toggle-btn-type");
+//   if (filterList.style.display === "none") {
+//     filterList.style.display = "flex";
+//     toggleBtn.textContent = "−";
+//   } else {
+//     filterList.style.display = "none";
+//     toggleBtn.textContent = "+";
+//   }
+// }
+
+// function toggleCostFilter() {
+//   let filterList = document.getElementById("weapon-cost");
+//   let toggleBtn = document.getElementById("toggle-btn-cost");
+//   if (filterList.style.display === "none") {
+//     filterList.style.display = "flex";
+//     toggleBtn.textContent = "−";
+//   } else {
+//     filterList.style.display = "none";
+//     toggleBtn.textContent = "+";
+//   }
+// }
+
+
